f0421dfcc9cea8aa24fed3e0fa764e1f858d62b3
Nelson Malbone <nhmalbone@icloud.com>
2026-01-04 14:20:16 -0500

    pdf generator. basic format for creating a basic pdf file

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ from fpdf import FPDF
 
 # create the instance
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+# adding a page
 pdf.add_page()
 
+# font for page
+
+pdf.set_font('Arial', 'B', size=12)
+
+# creating a cell
+pdf.cell(0, 12, txt='Hello World!', ln=1, align='l')
+
+pdf.output('test.pdf')

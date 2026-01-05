a5b8059c04eada7d335507f3e439a13ed830c8fb
Nelson Malbone <nhmalbone@icloud.com>
2026-01-05 13:59:21 -0500

    pdf generator. set up the code to read the "Topic" section of the csv file. now we generate 39 pages for each title.

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 # pdf generator.
 from fpdf import FPDF
+import pandas as pd
+
 
 # create the instance
 pdf = FPDF(orientation='P',
            unit='mm',
            format='A4')
+# getting data from csv file
+df = pd.read_csv("topics.csv")
 
-# adding a page
-pdf.add_page()
+for index, row in df.iterrows():
+    # adding a page
+    pdf.add_page()
 
-# font for page
+    # font for page
+    pdf.set_font('Arial',
+                 'B',
+                 size=12)
 
-pdf.set_font('Arial',
-             'B',
-             size=12)
-
-# creating a cell
-pdf.cell(0,
-         12,
-         txt='Hello World!',
-         ln=1,
-         align='l',
-         border='B')
-
+    # creating a cell
+    pdf.cell(0,
+             12,
+             txt=row["Topic"],
+             ln=1,
+             align='l',
+             border='B')
 pdf.output('test.pdf')

3766ff8e1258d078a0ee4f39b1eb12793c185b51
Nelson Malbone <nhmalbone@icloud.com>
2026-01-06 12:47:23 -0500

    pdf generator. added the footer for both main and sub pages of pdf file

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ import pandas as pd
 pdf = FPDF(orientation='P',
            unit='mm',
            format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 # getting data from csv file
 df = pd.read_csv("topics.csv")
 
+# parent page
 for index, row in df.iterrows():
     # adding a page
     pdf.add_page()
 
+    # set for header
     # font for page
     pdf.set_font('Arial',
                  'B',
@@ -27,7 +30,19 @@ for index, row in df.iterrows():
              align='l',
              border='B')
 
+    #set the footer
+    pdf.ln(265)
+    pdf.set_font('Arial', style='i', size=10)
+    pdf.cell(0, 10, txt=row["Topic"], ln=1, align='R')
+
+
+    # additional page count
     for x in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # set the footer
+        pdf.ln(276)
+        pdf.cell(0, 10, txt=row["Topic"], ln=1, align='R')
+        pdf.set_font('Arial', style='i', size=10)
 
 pdf.output('test.pdf')
